c31ce717c97e5e16665fda7f6d8e4fafacf035e3
Sjur Millidahl <sjur.millidahl@finn.no>
2018-01-02 14:14:22 +0100

    Started on part 1 - types and data

--- src/Part1.elm
+++ src/Part1.elm
@@ -1,3 +1,40 @@
 module Part1 exposing (..)
 
-multBy2 x = x * 2
+{--
+  Complete this newly invented type caled `Shape`.
+  It should have two Data Constructors that replace the dummy datatype `Todo`
+  One should be called `Circle` and contain a Float to represent a diameter in cm
+  The other should be called `Square` and contain a Float to represent one of (the equal) side lengths in cm
+--}
+-- type Shape = Todo
+type Shape = Circle Float | Square Float
+
+size : Shape -> Float
+size shape =
+  case shape of
+          Circle diameter -> diameter
+          Square side -> side
+
+{--
+  Complete this function that accepts a Shape, and returns a Circle.
+  Requirements :
+    - Given a Circle, it should be returned unchanged
+    - Given a Square, it should return a Circle as wide as the Square
+--}
+toCircle : Shape -> Shape
+toCircle shape =
+  case shape of
+          Circle x -> shape
+          Square x -> Circle x
+
+{--
+  Complete this function that accepts a Shape, and returns a Square.
+  Requirements :
+    - Given a Square, it should be returned unchanged
+    - Given a Circle, it should return a Square as wide as the Circle
+--}
+toSquare : Shape -> Shape
+toSquare shape =
+  case shape of
+          Square x -> shape
+          Circle x -> Square x
--- tests/TestSuite.elm
+++ tests/TestSuite.elm
@@ -8,8 +8,14 @@ import Debug exposing (..)
 suite : Test
 suite =
     describe "Part1 Exercises"
-      [ test "dummytest" <|
+      [ test "circle converts to square" <|
           \_ ->
-                let tuples = [(2,0),(1,2)]
-                in Expect.equal [(2,0),(1,2)] tuples
-    ]
+                let circle = Circle 1.0
+                    square = Square 1.0
+                in Expect.equal square <| Part1.toSquare circle
+        ,test "square converts to circle" <|
+          \_ ->
+                let circle = Circle 1.0
+                    square = Square 1.0
+                in Expect.equal circle <| Part1.toCircle square
+      ]
