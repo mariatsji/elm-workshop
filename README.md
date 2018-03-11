22c0ceb11969608c2ed6780e66c3e75b08dd99ec
Sjur Millidahl <sjur.millidahl@finn.no>
2018-03-11 19:10:53 +0100

    create elm slides with elm-slides

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Download elm for your platform : https://guide.elm-lang.org/install.html
 Download elm-test using npm : https://www.npmjs.com/package/elm-test
 (Download nodejs first if you are missing npm : https://www.npmjs.com/get-npm)
+
 Use an IDE with Elm syntax highlighting. Choices :
 - Intellij with Elm Language Plugin
 - Atom with elm-package
@@ -9,3 +10,6 @@ Use an IDE with Elm syntax highlighting. Choices :
 
 # Run tests
     elm test --watch
+
+# Run slides
+  ./serve.sh
---
+++ src/Examples.elm
@@ -0,0 +1,75 @@
+import Html exposing (Html, Attribute, beginnerProgram, text, div, input, li, ul)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import String
+
+{--
+See how `listAligned` is used.
+
+Task 1 : refactor the view to reverse a text (see Example 1)
+Task 2 : create your own text transformation, any way you like.
+  You can do this by creating a new function with the signature Model -> Html msg
+  And applying this function from `view`
+--}
+
+main =
+  beginnerProgram { model = "", view = view, update = update }
+
+-- MODEL
+type alias Model = String
+
+-- UPDATE
+
+type Msg = NewContent String
+
+update : Msg -> Model -> Model
+update (NewContent content) oldContent =
+  content
+
+{--
+Example 1
+--}
+reverseString : Model -> Html msg
+reverseString content = text (String.reverse content)
+
+{--
+Example 2
+--}
+listAligned : Model -> Html msg
+listAligned content
+  = let w = String.words content
+        pad = String.padLeft 20 '.'
+        padded = List.map pad w
+        toLi = \x -> li [] [ text x ]
+    in ul [] <| List.map toLi padded
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view content =
+  div []
+    [ input [ placeholder "Text to edit", onInput NewContent, inputStyle ] []
+    , div [ outputStyle ] [ listAligned content ]
+    ]
+
+inputStyle : Html.Attribute msg
+inputStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "40px")
+    , ("padding", "10px 0")
+    , ("font-size", "2em")
+    , ("text-align", "center")
+    ]
+
+outputStyle : Html.Attribute msg
+outputStyle =
+  style
+    [ ("width", "100%")
+    , ("height", "40px")
+    , ("padding", "0")
+    , ("font-size", "2em")
+    , ("text-align", "left")
+    , ("font-family", "monospace")
+    ]
